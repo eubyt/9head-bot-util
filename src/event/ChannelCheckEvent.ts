44a99a2f92ac2c179665d11496888b9419adf69d
eubyt <adrian@eubyt.dev>
2025-01-18 22:23:33 -0300

    Fix delete

--- src/event/ChannelCheckEvent.ts
+++ src/event/ChannelCheckEvent.ts
@@ -22,6 +22,7 @@ export class ChannelCheckEvent implements EventHandler<'Message'> {
 
     async execute(message: Message): Promise<void> {
         const fmRegexCommand = /\.(f|gw|r|c|u|judge)(\s?.*)?/;
+        const command = message.content.split(' ')[0];
 
         if (message.author.bot || !message.guildId) return;
 
