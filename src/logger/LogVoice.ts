2be2f8cd21f4581ec34a9ae9f54acbdcd9d8e6bb
eubyt <adrian@eubyt.dev>
2025-02-13 12:18:21 -0300

    Test flag

--- src/logger/LogVoice.ts
+++ src/logger/LogVoice.ts
@@ -24,6 +24,7 @@ export async function logVoice(
             content: `<@${user.id}>`,
             embeds: [embed],
             files: [avatarAttachment],
+            flags: [4096],
         });
     };
 
